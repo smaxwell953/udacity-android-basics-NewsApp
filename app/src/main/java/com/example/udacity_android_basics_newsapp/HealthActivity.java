1a66da7c2fb29ac84e2bd087544cee7b1691fd68
Sara <saraann.maxwell@gmail.com>
2020-05-03 06:21:21 -0600

    Unsuccessful attempt to get "no internet" to display

--- app/src/main/java/com/example/udacity_android_basics_newsapp/HealthActivity.java
+++ app/src/main/java/com/example/udacity_android_basics_newsapp/HealthActivity.java
@@ -196,45 +196,21 @@ public class HealthActivity extends AppCompatActivity
 
     @Override
     public void onLoadFinished(android.content.Loader<List<Health>> loader, List<Health> healtharticles) {
-        // Get a reference to the ConnectivityManager to check state of network connectivity
-        ConnectivityManager connMgr = (ConnectivityManager)
-                getSystemService(Context.CONNECTIVITY_SERVICE);
+        View loadingIndicator = findViewById(R.id.loading_indicator);
+        loadingIndicator.setVisibility(View.GONE);
 
-        // Get details on the currently active default data network
-        NetworkInfo networkInfo = null;
-        if (connMgr != null) {
-            networkInfo = connMgr.getActiveNetworkInfo();
-        }
+        // Update empty state with no connection error message
+        mEmptyStateTextView.setText(R.string.no_internet_connection);
+        mEmptyStateTextView.setVisibility(View.VISIBLE);
 
-        // If there is a network connection, fetch data
-        if (networkInfo != null && networkInfo.isConnected()) {
-            // Get a reference to the LoaderManager, in order to interact with loaders.
-            LoaderManager loaderManager = getLoaderManager();
-
-            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
-            // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
-            // because this activity implements the LoaderCallbacks interface).
-            loaderManager.initLoader(HEALTH_LOADER_ID, null, this);
-        } else {
-            // Otherwise, display error
-            // First, hide loading indicator so error message will be visible
-            View loadingIndicator = findViewById(R.id.loading_indicator);
-            loadingIndicator.setVisibility(View.GONE);
-
-            // Update empty state with no connection error message
-            mEmptyStateTextView.setText(R.string.no_internet_connection);
-            mEmptyStateTextView.setVisibility(View.VISIBLE);
-        }
-
-        // Clear the adapter of previous health article data
-        mAdapter.clear();
+        getLoaderManager().destroyLoader(HEALTH_LOADER_ID);
 
         // If there is a valid list of {@link Health Article}s, then add them to the adapter's
         // data set. This will trigger the ListView to update.
         if (healtharticles != null && !healtharticles.isEmpty()) {
             mAdapter.addAll(healtharticles);
         }
-        getLoaderManager().destroyLoader(HEALTH_LOADER_ID);
+
     }
 
     @Override
